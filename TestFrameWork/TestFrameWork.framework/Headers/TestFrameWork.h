8e9ecebd1c844893c9f1c08496c0383125b4ffe5
fengbing <fengbing@g42pay.com>
2020-06-27 01:40:23 +0400

    upload info

---
+++ TestFrameWork/TestFrameWork.framework/Headers/TestFrameWork.h
@@ -0,0 +1,20 @@
+//
+//  TestFrameWork.h
+//  TestFrameWork
+//
+//  Created by ice on 2020/6/27.
+//  Copyright © 2020 ice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFrameWork.
+FOUNDATION_EXPORT double TestFrameWorkVersionNumber;
+
+//! Project version string for TestFrameWork.
+FOUNDATION_EXPORT const unsigned char TestFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameWork/PublicHeader.h>
+#import <TestFrameWork/TestLog.h>
+
+
---
+++ TestFrameWork/TestFrameWork.framework/Headers/TestLog.h
@@ -0,0 +1,17 @@
+//
+//  TestLog.h
+//  TestFrameWork
+//
+//  Created by ice on 2020/6/27.
+//  Copyright © 2020 ice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestLog : NSObject
++ (void)login;
+@end
+
+NS_ASSUME_NONNULL_END
