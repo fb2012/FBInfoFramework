4408d14dd90f1f30be147163ff6a633826a13c7b
fengbing <fengbing@g42pay.com>
2020-06-27 16:00:53 +0400

    update info 0.4

---
+++ FBTestFrameInfo/FBTestFrameInfo.framework/Headers/FBTestLog.h
@@ -0,0 +1,17 @@
+//
+//  FBTestLog.h
+//  FBTestFrameInfo
+//
+//  Created by ice on 2020/6/27.
+//  Copyright © 2020 ice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FBTestLog : NSObject
++ (void)testLog;
+@end
+
+NS_ASSUME_NONNULL_END
